15ec4411f38ba53e8bbdf6d74949f50ca6bc0777
arto-r <artoghrul.rashid23@fryeburgacademy.org>
2022-08-31 21:59:04 +0400

    trytest3

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,10 +5,10 @@ const Navbar = () => {
         // <Router>
             <div id="navbar" className="topnav">
                 <ul>
-                    <li><Link to='/facs'>Home</Link></li>
+                    <li><Link to='https://arto-r.github.io/facs/'>Home</Link></li>
                     <li><Link to='https://arto-r.github.io/facs/courses'>Courses</Link></li>
                     <div className="right">
-                        <li><Link to='https://arto-r.github.io/facs/signu'>Sign Up</Link></li>
+                        <li><Link to='https://arto-r.github.io/facs/signup'>Sign Up</Link></li>
                         <li><Link to='https://arto-r.github.io/facs/login'>Login</Link></li>
                     </div>
                 </ul>
