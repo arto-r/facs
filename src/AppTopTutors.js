c11e3fa05e4b0e84a51d43ed4338596961d4d92c
arto-r <artoghrul.rashid23@fryeburgacademy.org>
2022-09-02 19:56:26 +0400

    textDecoration: 'none'

--- src/AppTopTutors.js
+++ src/AppTopTutors.js
@@ -6,7 +6,7 @@ const AppTopTutors = () => {
         <div className="toptutors">
             <h1 style={{textAlign: 'center',position:'relative',top:'25px'}}><Link to='/vote' style={{color:'white'}}>Top tutors of the month</Link></h1>
             <div className="notheading">
-                <Link to="/profile">
+                <Link to="/profile" style={{textDecoration:"none"}}>
                     <div className="toptutor" id="topnum2">
                         <h3 className="nh2">#2</h3>
                         <div className="n2">
@@ -15,7 +15,7 @@ const AppTopTutors = () => {
                         </div>
                     </div>
                 </Link>
-                <Link to="/profile">
+                <Link to="/profile" style={{textDecoration:"none"}}>
                     <div className="toptutor" id="topnum1">
                         <h3 className="nh1">#1</h3>
                         <div className="n1">
@@ -24,7 +24,7 @@ const AppTopTutors = () => {
                         </div>
                     </div>
                 </Link>
-                <Link to="/profile">
+                <Link to="/profile" style={{textDecoration:"none"}}>
                     <div className="toptutor" id="topnum3">
                         <h3 className="nh3">#3</h3>
                         <div className="n3">
