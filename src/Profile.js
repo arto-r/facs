b0643c1982361e4f09085e48a2b9cace99cf226c
arto-r <artoghrul.rashid23@fryeburgacademy.org>
2022-09-04 13:24:12 +0400

    profile.css

--- src/Profile.js
+++ src/Profile.js
@@ -1,8 +1,18 @@
 import Navbar from "./Navbar";
+import Footer from "./Footer"
+import './Profile.css'
 
 const Profile = () => {
     return (
-        <Navbar />
+        <>
+            <Navbar />
+            <div className="everything">
+                <div className="teacher-profile">
+                    <img className="selfie-profile" src={require("./assets/selfie.jpg")} alt="selfie.jpg"/>
+                </div>
+            </div>
+            <Footer />
+        </>
     );
 }
 
